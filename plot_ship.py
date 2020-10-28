3b8fb56afd26ac6c941175d38d2f923d47788d2c
Bjoern <BHFock@gmail.com>
2020-10-28 18:49:41 +0000

    Add point at la gomera

--- plot_ship.py
+++ plot_ship.py
@@ -9,15 +9,14 @@ import matplotlib.pyplot as plt
 lat=np.array([43.53,43.49,43.45,43.42,43.39,43.38,43.37,43.37,43.38,43.4])
 lon=np.array([-116.16,-116.17,-116.23,-116.29,-116.36,-116.44,-116.52,-116.6,-116.68,-116.76])
 
-
 lat_exmouth=50.62
 lon_exmouth=-3.4137
 
 lat_la_gomera=28.1033 
-lon_la_gomera=17.2194
+lon_la_gomera=-17.2194
 
 #define map extent
-extent = [-25, 5, 30, 54]
+extent = [-20, 5, 25, 52.5]
 
 #define state borders
 states_borders = cfeature.NaturalEarthFeature(
@@ -32,18 +31,29 @@ states_provinces = cfeature.NaturalEarthFeature(
         scale='50m',
         facecolor='none')
 
-
 #create figure
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
 #Add features
-ax.add_feature(cfeature.LAND)
-ax.add_feature(states_provinces, edgecolor='gray')
-ax.add_feature(states_borders, edgecolor='black')
+#ax.stock_img()
+#ax.coastlines()
+
+#ax.add_feature(cfeature.LAND)
+#ax.add_feature(states_provinces, edgecolor='gray')
+#ax.add_feature(states_borders, edgecolor='black')
+ax.gridlines()
+ax.coastlines(resolution='50m')
+
+
+
 #plot data
 
 ax.plot(lon_exmouth, lat_exmouth, marker='o', color='red', markersize=12,
         alpha=0.7, transform=ccrs.PlateCarree())
+
+ax.plot(lon_la_gomera, lat_la_gomera, marker='o', color='red', markersize=12,
+        alpha=0.7, transform=ccrs.PlateCarree())
+
 
 ax.plot(lon,lat, 'o',transform=ccrs.PlateCarree())
 ax.set_extent(extent)
