a16fb9be7d9e8fd4c891b6e7c05be7ac13a49d57
BHFock <bhfock@gmail.com>
2020-11-07 13:51:09 +0000

    correct fallback initialisation for last_date if no data is read from the rowing log for the current date

--- src/vrt_read_logbook.py
+++ src/vrt_read_logbook.py
@@ -7,13 +7,13 @@ def read_logbook(ifile, d1=None, d2=None):
     import datetime as dt
 
     df = pandas.read_csv(ifile, sep=' *; *', engine='python')
-
     pandas.to_datetime(df['date'])
     if d1 is not None and d2 is not None:
         df = df.loc[(df['date'] > d1) & (df['date'] <= d2)]
+
     if(df.empty):
         distance = 0
-        last_date = enddate
+        last_date = d2
     else:
         distance = df['meter'].sum(axis=0)
         last_date = dt.datetime.strptime(df['date'].values[-1], '%Y-%m-%d')
