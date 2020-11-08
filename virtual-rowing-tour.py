bbed8fa667b020347b9ffd545179ec294f05e711
BHFock <bhfock@gmail.com>
2020-11-08 20:36:59 +0000

    Add Team class to hold information about participating crews

--- virtual-rowing-tour.py
+++ virtual-rowing-tour.py
@@ -69,6 +69,56 @@ def main():
         ytick_inc_1 = None
         coast_alpha = [None, None]
 
+    class Team:
+        def __init__(self, config_file=None, boat=None, color=None,
+                     logbook=None, members=None, name=None):
+
+            import configparser
+
+            #config file
+            self.conf=config_file
+
+            # set default
+            self.boat="1x"
+            self.color="red"
+            self.members="NN"
+            self.name="NN"
+
+            # read config file
+            config = configparser.ConfigParser()
+            config.read("conf/" + config_file)
+            for key in config['team']:
+                if key=='boat':
+                    self.boat = config['team'][key]
+                if key=='color':
+                    self.color = config['team'][key]
+                if key=='logbook':
+                    self.logbook = config['team'][key]
+                if key=='members':
+                    self.members = config['team'][key]
+                if key=='name':
+                    self.name = config['team'][key]
+
+            # overwrite values from config file if specified
+            if boat:
+                self.boat = boat
+            if color:
+                self.color = color
+            if logbook:
+                self.logbook = logbook
+            if members:
+                self.members = members
+            if name:
+                self.name = name
+
+    crews = []
+    crews.append(Team(config_file="rowing.conf"))
+    crews.append(Team(config_file="land_crew.conf"))
+
+    #for crew in crews:
+    #    print(crew.name)
+    #sys.exit()
+
     # Read and calculate data
 
     final_date = dt.date.today().strftime("%Y-%m-%d")
